091495c507b586152651744660b80ad7c038fd55
Prakash_Singh <prakashsingh98310@gmail.com>
2024-11-15 05:18:44 +0530

    Nutrition-sec-created

--- CareSync/src/App.jsx
+++ CareSync/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-import Header from './components/Header/Header'
+// import Header from './components/Header/Header'
 import './App.css'
-import Home from './components/Home/Home'
+// import Home from './components/Home/Home'
+import MealTracker from './components/Nutritional-comp/Nutri-components/MealTracker.jsx';
+ // Adjust path if necessary
 function App() {
   
 
   return (
     <>
-    <Header/>
-    <Home/>
+    {/* <Header/>
+    <Home/> */}
+    <MealTracker />
     
     </>
   )
